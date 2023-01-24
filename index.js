282d5e51220ccbe701d796bb68fa21fffaf4be5e
Arturo1802 <trigodart45@gmail.com>
2023-01-24 12:40:03 -0600

    Third Commit

--- index.js
+++ index.js
@@ -25,35 +25,37 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/:date?",(req, res)=>{
-	var YeDaMo=req.params.date.split("-")
+
+
+	console.log(typeof req.params.date)
 	//undefined 
 	if(typeof req.params.date === 'undefined'){
-		var r=new Date(YeDaMo[0],YeDaMo[1],YeDaMo[2]); 
+		var r=new Date(); 
 		x=r.getTime()  
-		res.json({unix:x,utc:r.toString()})
+		res.json({unix:x,utc:r.toString().slice(0,28)})
 	}
+
 	//is unix
-	if(!isNaN(req.params.date)){ 
-	var r=new Date(req.params.date*1000); 
-	var x=new Date(req.params.date*1000).getTime() 
-	res.json({unix:x,utc:r.toString()})
+	console.log(!isNaN(req.params.date))
+	if(!(isNaN(req.params.date))){ 
+	var r=new Date(parseInt(req.params.date));  
+	console.log("Numero")
+	console.log(r)
+	res.json({unix:req.params.date,utc:r.toString().slice(0,28)})
 	
 	}else{
-	//normal date
-	var regExp = /[a-zA-Z]/u;
+	 
+		//normal date
+		var regExp = /[a-zA-Z]/u;
 		if(!regExp.test(req.params.date)){
-			var ut=new Date(YeDaMo[0],YeDaMo[1],YeDaMo[2]); 
-			console.log(ut)
-			var x=ut.getTime() 
-			 
-			res.json({unix:x,utc:ut.toString()}) 
+			var ut=new Date(req.params.date);  
+			var x=ut.getTime()  
+			res.json({unix:x,utc:ut.toString().slice(0,28)}) 
 		}else{
-		//Error
+			//Error 
 			res.json({error:"Invalid Date" })
 		}
-	}
-	
-	 
+	} 
 })
 
 
